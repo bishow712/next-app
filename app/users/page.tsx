ed8f3a4d27ff0ba81437fe23e31554a48503dbb2
Bishow <bishow712@gmail.com>
2024-08-12 08:32:09 +0545

    Components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { link } from 'fs';
+import React, { cache } from 'react'
 
-const UsersPage = () => {
-  return (
-    <div>
-      UsersPage
-    </div>
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache:'no-store'}) //{cache:'no-store'} Doesn't store in cache, {next: {revalidate: 10}} to refresh after certain seconds(10 seconds here)
+  const users: User[] = await res.json()
+  
+  return (    
+    <>
+        <h1>Users</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
+        <ul>
+            {users.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+    </>
   )
 }
 
